12b492ea4fc7add29ff93d1a71e6b93984f2a097
asiyasera248ml <asiyasera248@gmail.com>
2026-03-23 20:57:12 +0530

    Create app_v2.py

---
+++ app_v2.py
@@ -0,0 +1,10 @@
+# Fake News Detector
+
+This project detects fake and real news using Machine Learning.
+
+## Dataset
+Download dataset from:
+https://www.kaggle.com/datasets/clmentbisaillon/fake-and-real-news-dataset
+
+## How to run
+streamlit run ui.py
